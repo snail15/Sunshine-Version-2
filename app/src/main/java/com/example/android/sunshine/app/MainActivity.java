358cc271be82c6ae65506c9237fe6ff28deb2924
Sungin Jung <jungs@uchicago.edu>
2016-08-07 05:39:12 -0500

    settings

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.android.sunshine.app;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
