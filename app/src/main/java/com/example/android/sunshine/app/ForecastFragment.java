a4a88a9a214c1d978909033f701949361a9050c5
Sungin Jung <jungs@uchicago.edu>
2016-08-07 05:31:18 -0500

    settings

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -1,9 +1,11 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -62,7 +64,10 @@ public class ForecastFragment extends Fragment {
         switch (item.getItemId()){
             case R.id.action_refresh:
                 FetchWeatherTask weatherTask = new FetchWeatherTask();
-                weatherTask.execute("07093");
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                String location = prefs.getString(getString(R.string.pref_location_key),
+                                        getString(R.string.pref_location_default));
+                weatherTask.execute(location);
                 return true;
             case R.id.action_settings:
                 Intent settingsIntent = new Intent(getActivity(), SettingsActivity.class);
